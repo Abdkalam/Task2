287e505221a47edb16de3c9df6853d9de9807f74
Abdkalam <amjabdul@gmail.com>
2022-05-05 14:12:33 +0530

    new project

---
+++ src/org/sam/Variable.java
@@ -0,0 +1,25 @@
+package org.sam;
+
+public class Variable {
+	 int a=10;
+	private void var()
+	{
+		int a=120;
+		System.out.println("the value of "+a);
+	}
+	public  void var1() {
+		System.out.println(a);
+	}
+	
+	public static void main(String[] args) {
+	
+		Variable c=new Variable();
+		System.out.println(c.a);
+		c.var();
+		System.out.println(c.a);
+		Variable c1=new Variable();
+		c1.var();
+		System.out.println(c1.a);
+	}
+
+}
